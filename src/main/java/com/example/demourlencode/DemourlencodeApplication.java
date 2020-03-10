cfac05862f40b45b39aa1bbaf816fff899498b77
debraj1976 <De365600@@>
2020-03-09 23:21:52 -0500

    initial commit

---
+++ src/main/java/com/example/demourlencode/DemourlencodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demourlencode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemourlencodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemourlencodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demourlencode/DemourlencodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demourlencode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemourlencodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
